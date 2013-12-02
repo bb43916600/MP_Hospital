8a2184572b58f1defc537be39c69ce403504f07c
bb43916600 <43916600@qq.com>
2013-12-02 16:35:48 +0800

    Revert "删除测试"
    This reverts commit 9fb9c7e9f27a5be697e0d6dfcd604f57007ea19d.

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MP_Hospital
+//
+//  Created by 龙 华雄 on 13-12-2.
+//  Copyright (c) 2013年 龙 华雄. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
